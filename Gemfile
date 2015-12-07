108b0073d0eb080bf1632d6717012472409759b4
Chad Scribner <chad.scribner@firespring.com>
2015-12-07 15:19:17 -0600

    Adding sandp dow and 10 year.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem 'dashing'
 gem 'finance-ytd'
 gem 'quote-only'
+gem 'nokogiri' # todo remove this
--- jobs/finance.rb
+++ jobs/finance.rb
@@ -7,10 +7,16 @@ def cnn_etf(symbol, friendly_name)
 	calc_send(f.symbol, f.friendly_name, q.quote, f.ytd_return, f.symbol.upcase)
 end
 
+def cnn_market(symbol, friendly_name, moreinfo)
+	f = CnnMarketFinanceYtd.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => 2 })
+	q = CnnMarketQuoteOnly.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => 2 })
+	calc_send(f.symbol, f.friendly_name, q.quote, f.ytd_return, moreinfo)
+end
+
 def calc_send(id, title, current, ytd_return, moreinfo)
 	last = current / (1.0 + ytd_return)
 	status = 'up'
-	
+
 	if current < last
 		status = 'down'
 	end
@@ -27,14 +33,21 @@ def do_job
 	q = ApmexGoldQuoteOnly.new({ :symbol => 'oz', :friendly_name => 'Gold', :decimal_places => 2 })
 	calc_send(f.symbol.downcase, f.friendly_name, q.quote, f.ytd_return, 'per oz')
 
-	f = ApmexSilverFinanceYtd.new({ :symbol => 'Silver', :friendly_name => 'Silver', :decimal_places => 2, :price_last_year => 15.56 })	
+	f = ApmexSilverFinanceYtd.new({ :symbol => 'Silver', :friendly_name => 'Silver', :decimal_places => 2, :price_last_year => 15.56 })
 	q = ApmexSilverQuoteOnly.new({ :symbol => 'oz', :friendly_name => 'Silver', :decimal_places => 2 })
 	calc_send(f.symbol.downcase, f.friendly_name, q.quote, f.ytd_return, 'per oz')
 
-	cnn_etf('vti', 'US Stocks');	
+	cnn_etf('vti', 'US Stocks');
 	cnn_etf('vxus', 'Foreign Stocks');
 	cnn_etf('sgdm', 'Gold Miners');
 	cnn_etf('bwz', 'Foreign Cash');
+	cnn_market('dow', 'Dow', 'DJIA');
+	cnn_market('sandp', 'S&P', 'S&P 500 Index');
+
+	f = BloombergFinanceYtd.new({ :symbol => 'USGG10YR:IND', :friendly_name => '10 Year', :decimal_places => 2 })
+	q = BloombergQuoteOnly.new({ :symbol => 'USGG10YR:IND', :friendly_name => '10 Year', :decimal_places => 0 })
+	calc_send('USGG10YRIND', f.friendly_name, q.quote, f.ytd_return, 'Treasury')
+
 	cnn_etf('xbt', 'Bitcoin');
 end
 
