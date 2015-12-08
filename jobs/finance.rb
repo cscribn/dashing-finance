0e81ec667c86a22324791ba1c46b871fae1b3dd8
Chad Scribner <chad.scribner@firespring.com>
2015-12-08 16:03:10 -0600

    Fixing naming.

--- jobs/finance.rb
+++ jobs/finance.rb
@@ -1,8 +1,8 @@
 require 'finance-ytd'
 require 'quote-only'
 
-def cnn_etf(symbol, friendly_name)
-	f = CnnEtfFinanceYtd.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => 2 })
+def cnn(symbol, friendly_name)
+	f = CnnFinanceYtd.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => 2 })
 	q = CnnQuoteOnly.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => 2 })
 	calc_send(f.symbol, f.friendly_name, q.quote, f.ytd_return, f.symbol.upcase)
 end
@@ -27,7 +27,7 @@ def calc_send(id, title, current, ytd_return, moreinfo)
 end
 
 def do_job
-	cnn_etf('vt', 'World Stocks');
+	cnn('vt', 'World Stocks');
 
 	f = ApmexGoldFinanceYtd.new({ :symbol => 'Gold', :friendly_name => 'Gold', :decimal_places => 2, :price_last_year => 1183.90 })
 	q = ApmexGoldQuoteOnly.new({ :symbol => 'oz', :friendly_name => 'Gold', :decimal_places => 2 })
@@ -37,10 +37,10 @@ def do_job
 	q = ApmexSilverQuoteOnly.new({ :symbol => 'oz', :friendly_name => 'Silver', :decimal_places => 2 })
 	calc_send(f.symbol.downcase, f.friendly_name, q.quote, f.ytd_return, 'per oz')
 
-	cnn_etf('vti', 'US Stocks');
-	cnn_etf('vxus', 'Foreign Stocks');
-	cnn_etf('sgdm', 'Gold Miners');
-	cnn_etf('bwz', 'Foreign Cash');
+	cnn('vti', 'US Stocks');
+	cnn('vxus', 'Foreign Stocks');
+	cnn('sgdm', 'Gold Miners');
+	cnn('bwz', 'Foreign Cash');
 	cnn_market('dow', 'Dow', 'DJIA');
 	cnn_market('sandp', 'S&P', 'S&P 500 Index');
 
@@ -48,7 +48,7 @@ def do_job
 	q = BloombergQuoteOnly.new({ :symbol => 'USGG10YR:IND', :friendly_name => '10 Year', :decimal_places => 0 })
 	calc_send('USGG10YRIND', f.friendly_name, q.quote, f.ytd_return, 'Treasury')
 
-	cnn_etf('xbt', 'Bitcoin');
+	cnn('xbt', 'Bitcoin');
 end
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
