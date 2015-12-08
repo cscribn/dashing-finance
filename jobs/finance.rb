5b71707482fc97c1912d9c115b428567147e061d
Chad Scribner <chad.scribner@firespring.com>
2015-12-08 15:57:52 -0600

    Updating gems.  Removing debugging.

--- jobs/finance.rb
+++ jobs/finance.rb
@@ -14,7 +14,6 @@ def cnn_market(symbol, friendly_name, moreinfo)
 end
 
 def calc_send(id, title, current, ytd_return, moreinfo)
-	puts "ytd_return: #{ytd_return}"
 	last = current / (1.0 + ytd_return)
 	status = 'up'
 
